3fd1bd8c37db0aeb02c6026dbed7cd3977fe5981
Kirill Smirnov <KSmirnov.mail@gmail.com>
2023-06-15 14:41:03 -0400

    Initial Commit

---
+++ CryptoData/CryptoDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoDataApp.swift
+//  CryptoData
+//
+//  Created by Kirill Smirnov on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
